c4d7d36d2e831ee9679671a2203846ff55e56f6d
Tambet Matiisen <tambet.matiisen@gmail.com>
2018-03-12 12:02:29 +0200

    EVA and radio news combined.

--- eki_preprocess/combine.py
+++ eki_preprocess/combine.py
@@ -5,17 +5,21 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input_dir')
+parser.add_argument('--speaker_id', type=int, default=7)
 args = parser.parse_args()
 
-with open(os.path.join(args.input_dir, 'train.txt'), 'w') as of:
+with open(os.path.join(args.input_dir, 'train.txt'), 'w', encoding='utf-8') as of:
     csvwriter = csv.writer(of, delimiter='|', quotechar='*')
     for dir in glob.glob(os.path.join(args.input_dir, '*')):
         if not os.path.isdir(dir):
             continue
         reldir = os.path.relpath(dir, args.input_dir)
         with open(os.path.join(dir, 'train.txt'), encoding='utf-8') as f:
-            csvreader = csv.reader(f, delimiter='|')
+            csvreader = csv.reader(f, delimiter='|', quotechar='*')
             for data in csvreader:
                 data[0] = os.path.join(reldir, data[0])
                 data[1] = os.path.join(reldir, data[1])
+                if len(data) == 4:
+                    data.append(args.speaker_id)
+                assert len(data) == 5
                 csvwriter.writerow(data)
